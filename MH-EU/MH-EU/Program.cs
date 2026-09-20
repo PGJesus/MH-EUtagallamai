2237972f519490e42c717e80ac2b12165a920a14
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2026-09-20 19:49:37 +0200

    done

--- MH-EU/MH-EU/Program.cs
+++ MH-EU/MH-EU/Program.cs
@@ -42,41 +42,35 @@ namespace EU
             }
         }
 
-        // 3. feladat: hány tagállama volt 2018-ban az Európai Uniónak
         public void TagallamokSzama()
         {
             Console.WriteLine($"3. feladat: EU tagállamainak száma: {orszagok.Count} db");
         }
 
-        // 4. feladat: 2007-ben csatlakozott országok száma
         public void Csatlakozas2007Ben()
         {
             int szam = orszagok.Count(o => o.Csatlakozas.Year == 2007);
             Console.WriteLine($"4. feladat: 2007-ben {szam} ország csatlakozott.");
         }
 
-        // 5. feladat: Magyarország csatlakozási dátuma
         public void MagyarorszagCsatlakozasa()
         {
             var magyarorszag = orszagok.First(o => o.Nev == "Magyarország");
             Console.WriteLine($"5. feladat: Magyarország csatlakozásának dátuma: {magyarorszag.Csatlakozas:yyyy.MM.dd}");
         }
 
-        // 6. feladat: volt-e májusi csatlakozás
         public void VoltEMajusiCsatlakozas()
         {
             bool volt = orszagok.Any(o => o.Csatlakozas.Month == 5);
             Console.WriteLine(volt ? "6. feladat: Májusban volt csatlakozás!" : "6. feladat: Májusban nem volt csatlakozás!");
         }
 
-        // 7. feladat: utoljára csatlakozott tagállam
         public void UtoljaraCsatlakozottOrszag()
         {
             var utolso = orszagok.OrderByDescending(o => o.Csatlakozas).First();
             Console.WriteLine($"7. feladat: Legutoljára csatlakozott ország: {utolso.Nev}");
         }
 
-        // 8. feladat: évenkénti statisztika a csatlakozott országok számáról
         public void EvenkentiStatisztika()
         {
             Console.WriteLine("8. feladat: Statiszika");
